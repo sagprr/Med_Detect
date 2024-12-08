dadef47facd349fc130a7a068b13ee435b827ee0
Sagar Panwar <113618122+sagprr@users.noreply.github.com>
2024-12-08 15:58:20 +0530

    Update ML_spy.py

--- ML_spy.py
+++ ML_spy.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/diabetes_model.sav', 'rb'))
+#diabetes_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/heart_disease_model.sav','rb'))
+#heart_disease_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/parkinsons_model.sav', 'rb'))
+#parkinsons_model = pickle.load(open('/Users/sagarpanwar/Desktop/Hackhaven/saved models/parkinsons_model.sav', 'rb'))
 
 
 
